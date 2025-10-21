48b3133e68d433a1e85c62d87f005ff43e8a151d
hoangnam150303 <hoangnam150303@gmail.com>
2025-10-21 14:18:22 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,23 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
-    "process.env": {}, // ✅ tránh lỗi "process is not defined"
+    "process.env": {}, // tránh lỗi "process is not defined"
   },
   build: {
     lib: {
       entry: "./src/main.jsx",
       name: "ChatWidget",
-      fileName: () => "chat-widget.js", // ✅ ép xuất ra 1 file duy nhất ở root
-      formats: ["iife"],
+      fileName: () => "chat-widget.js",
+      formats: ["iife"], // build dạng IIFE để nhúng
     },
+    cssCodeSplit: false, 
     rollupOptions: {
       output: {
-        dir: "dist", // ✅ tất cả nằm ở dist
         inlineDynamicImports: true,
-        assetFileNames: "[name].[ext]", // ✅ không tạo thư mục assets/
+        assetFileNames: "[name].[ext]", // không tạo thư mục assets/
       },
     },
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
